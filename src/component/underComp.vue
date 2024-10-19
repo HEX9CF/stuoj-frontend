e1af0b541d1946d4e8e70e2a556c0a18af6fa4d1
U.Nowen <1261174783@qq.com>
2024-10-19 17:33:25 +0800

    初始化

---
+++ src/component/underComp.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts" name="underComp">
+
+</script>
+
+<template>
+  <div class="under">
+  <a href="#">相关地址</a>
+    <a href="#">互助网站</a>
+  </div>
+</template>
+
+<style scoped>
+    .under{
+      display: flex;
+      justify-content: center;
+      text-align: center;
+      //font-family: "Bodoni MT",serif;
+      font-size: 15px;
+
+    }
+    .under a{
+      text-decoration: none;
+      color: #aaaaaa;
+      padding-right: 10px;
+    }
+    .under a:hover{
+      text-decoration: underline;
+    }
+    .under a:active{
+      text-decoration: underline;
+      color: #5e5c5c;
+    }
+
+</style>
