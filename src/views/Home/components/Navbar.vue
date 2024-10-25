f0c68c9248da18d8169043e84dcd97bb7b21f166
U.Nowen <1261174783@qq.com>
2024-10-25 15:13:15 +0800

    使用computed检测登录状态 导航链接使用list

--- src/views/Home/components/Navbar.vue
+++ src/views/Home/components/Navbar.vue
@@ -1,28 +1,24 @@
 <template>
   <header>
     <div class="top-bar">
-      <div class="logo">
-        <span>JNOJ</span>
-      </div>
+      <div class="logo"><span>JNOJ</span></div>
       <div class="title">Online Judge</div>
     </div>
     <nav>
       <div class="brand">JNOJ</div>
       <ul>
-        <li><router-link to="/" active-class="active-link"><i class="icon-home"></i> 首页</router-link></li>
-        <li><router-link to="/problem" active-class="active-link"><i class="icon-list"></i> 问题列表</router-link></li>
-        <li><router-link to="/status" active-class="active-link"><i class="icon-status"></i> 状态</router-link></li>
-        <li><router-link to="/ranking" active-class="active-link"><i class="icon-ranking"></i> 排行榜</router-link></li>
-        <li><router-link to="/group" active-class="active-link"><i class="icon-group"></i> 小组</router-link></li>
-        <li><router-link to="/contest" active-class="active-link"><i class="icon-contest"></i> 比赛</router-link></li>
-        <li><router-link to="/help" active-class="active-link"><i class="icon-help"></i> 帮助</router-link></li>
+        <li v-for="link in links" :key="link.path">
+          <router-link :to="link.path" active-class="active-link" :aria-label="link.label">
+            <i :class="link.icon"></i> {{ link.label }}
+          </router-link>
+        </li>
         <template v-if="isLoggedIn">
-          <li><router-link to="/profile" active-class="active-link"><i class="icon-user"></i> 个人</router-link></li>
-          <li><a @click="logout" href="#"><i class="icon-logout"></i> 登出</a></li>
+          <li><router-link to="/profile" active-class="active-link" aria-label="个人"><i class="icon-user"></i> 个人</router-link></li>
+          <li><a @click="logout" href="#" aria-label="登出"><i class="icon-logout"></i> 登出</a></li>
         </template>
         <template v-else>
-          <li><router-link to="/signup"><i class="icon-register"></i> 注册</router-link></li>
-          <li><router-link to="/login"><i class="icon-login"></i> 登录</router-link></li>
+          <li><router-link to="/signup" aria-label="注册"><i class="icon-register"></i> 注册</router-link></li>
+          <li><router-link to="/login" aria-label="登录"><i class="icon-login"></i> 登录</router-link></li>
         </template>
       </ul>
     </nav>
@@ -30,31 +26,28 @@
 </template>
 
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 
-const isLoggedIn = ref(false);
 const router = useRouter();
+const isLoggedIn = computed(() => localStorage.getItem('isLoggedIn') === 'true');
 
-const checkLoginStatus = () => {
-  isLoggedIn.value = localStorage.getItem('isLoggedIn') === 'true';
-};
+const links = [
+  { path: '/', label: '首页', icon: 'icon-home' },
+  { path: '/problem', label: '问题列表', icon: 'icon-list' },
+  { path: '/status', label: '状态', icon: 'icon-status' },
+  { path: '/ranking', label: '排行榜', icon: 'icon-ranking' },
+  { path: '/group', label: '小组', icon: 'icon-group' },
+  { path: '/contest', label: '比赛', icon: 'icon-contest' },
+  { path: '/help', label: '帮助', icon: 'icon-help' }
+];
 
 const logout = () => {
   localStorage.removeItem('isLoggedIn');
-  isLoggedIn.value = false;
   router.push('/login');
 };
-
-onMounted(() => {
-  checkLoginStatus();
-  window.addEventListener('storage', checkLoginStatus);
-});
-
-onBeforeUnmount(() => {
-  window.removeEventListener('storage', checkLoginStatus);
-});
 </script>
+
 
 <style scoped>
 header {
