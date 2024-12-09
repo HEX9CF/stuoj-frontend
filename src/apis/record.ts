42b055f762347818b074b12826987bbd5c49cbb9
ArtdragonXoX <1262922587@qq.com>
2024-12-09 11:31:34 +0800

    record API

--- src/apis/record.ts
+++ src/apis/record.ts
@@ -1,5 +1,34 @@
 import { useDefineApi } from "@/stores/useDefineApi"
-import type { Judgement,Submission } from "@/types/Record"
+import type { Judgement, Submission } from "@/types/Record"
 
-export const GetRecordeList=useDefineApi<
-{}
+export const getRecordeList = useDefineApi<
+    {
+        params: {
+            "end-time"?: string;
+            language?: number;
+            page?: number;
+            problem?: number;
+            size?: number;
+            "start-time"?: string;
+            status?: number;
+            user?: number;
+        }
+    },
+    Judgement
+>({
+    url: "/record",
+    method: "get"
+})
+
+export const getRecordeInfo = useDefineApi<
+    {
+        id: number
+    },
+    {
+        submission: Submission,
+        judgements: Judgement[]
+    }
+>({
+    url: "/record",
+    method: "get"
+})
