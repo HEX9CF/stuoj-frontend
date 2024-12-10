cd158f1044bd2b8d168c393f3125c26354884076
ArtdragonXoX <1262922587@qq.com>
2024-12-10 12:18:31 +0800

    Problem管理员级Api。

--- src/apis/problem.ts
+++ src/apis/problem.ts
@@ -1,5 +1,5 @@
 import { useDefineApi } from "@/stores/useDefineApi";
-import type { ProblemInfo, Solution, Tag } from "@/types/Problem";
+import type { FpsProblemInfo, FpsSolution, FpsTestCase, Global, ProblemHistory, ProblemInfo, Solution, Tag, Testcase } from "@/types/Problem";
 import type { Page } from "@/types/misc";
 
 export const getProblemListApi = useDefineApi<
@@ -41,3 +41,171 @@ export const getProblemTagsApi = useDefineApi<
     url: "/problem/tag",
     method: "get"
 });
+
+export const uploadProblemApi = useDefineApi<
+    {
+        data: ProblemInfo
+    },
+    number
+>({
+    url: "/admin/problem",
+    method: "post"
+})
+
+export const updateProblemApi = useDefineApi<
+    {
+        data: ProblemInfo
+    },
+    null
+>({
+    url: "/admin/problem",
+    method: "put"
+})
+
+export const deleteProblemApi = useDefineApi<
+    {
+        id: number
+    },
+    null
+>({
+    url: "/admin/problem",
+    method: "delete"
+})
+
+export const problemAddTagApi = useDefineApi<
+    {
+        data: {
+            problem_id: number,
+            tag_id: number
+        }
+    },
+    null
+>({
+    url: "/admin/problem/tag",
+    method: "post"
+})
+
+export const problemRemoveTagApi = useDefineApi<
+    {
+        data: {
+            problem_id: number,
+            tag_id: number
+        }
+    },
+    null
+>({
+    url: "/admin/problem/tag",
+    method: "delete"
+})
+
+export const getTestCaseApi = useDefineApi<
+    {
+        id: number
+    },
+    Testcase
+>({
+    url: "/admin/testcase",
+    method: "get"
+})
+
+export const uploadTestCaseApi = useDefineApi<
+    {
+        data: Testcase
+    },
+    number
+>({
+    url: "/admin/testcase",
+    method: "post"
+})
+
+export const updateTestCaseApi = useDefineApi<
+    {
+        data: Testcase
+    },
+    null
+>({
+    url: "/admin/testcase",
+    method: "put"
+})
+
+export const deleteTestCaseApi = useDefineApi<
+    {
+        id: number
+    },
+    null
+>({
+    url: "/admin/testcase",
+    method: "delete"
+})
+
+export const datamakeApi = useDefineApi<
+    Global,
+    string
+>({
+    url: "/admin/testcase/datamake",
+    method: "post"
+})
+
+export const getSolutionApi = useDefineApi<
+    {
+        id: number
+    },
+    Solution
+>({
+    url: "/admin/solution",
+    method: "get"
+})
+
+export const uploadSolutionApi = useDefineApi<
+    {
+        data: Solution
+    },
+    number
+>({
+    url: "/admin/solution",
+    method: "post"
+})
+
+export const updateSolutionApi = useDefineApi<
+    {
+        data: Solution
+    },
+    null
+>({
+    url: "/admin/solution",
+    method: "put"
+})
+
+export const deleteSolutionApi = useDefineApi<
+    {
+        id: number
+    },
+    null
+>({
+    url: "/admin/solution",
+    method: "delete"
+})
+
+export const getProblemHistoryApi = useDefineApi<
+    {
+        id: number
+    },
+    ProblemHistory
+>({
+    url: "/problem/history",
+    method: "get"
+})
+
+export const uploadFPSApi = useDefineApi<
+    {
+       
+    },
+    {
+        problem: FpsProblemInfo
+        solution: FpsSolution[]
+        testcases: FpsTestCase[]
+    }
+>({
+    url: "/admin/problem/fps",
+    method: "post"
+})
--- src/types/Problem.ts
+++ src/types/Problem.ts
@@ -25,9 +25,98 @@ export interface Tag {
 }
 
 export interface Solution {
-    id: number;
+    id?: number;
     language_id: number;
     problem_id: number;
     source_code: string;
+    [property: string]: any;
+}
+
+export interface Testcase {
+    id?: number;
+    problem_id: number;
+    serial: number;
+    test_input: string;
+    test_output: string;
+    [property: string]: any;
+}
+
+export interface Global {
+    rows: Row[];
+    [property: string]: any;
+}
+
+/**
+ * Row
+ */
+export interface Row {
+    row_size_id?: number;
+    values: Value[];
+    [property: string]: any;
+}
+
+/**
+ * Value
+ */
+export interface Value {
+    max?: number;
+    max_id?: number;
+    min?: number;
+    min_id?: number;
+    type: Type;
+    value_size_id?: number;
+    [property: string]: any;
+}
+
+export enum Type {
+    Float = "float",
+    Int = "int",
+}
+
+export interface ProblemHistory {
+    create_time: string;
+    description: string;
+    difficulty: number;
+    hint: string;
+    id: number;
+    input: string;
+    memory_limit: number;
+    operation: number;
+    output: string;
+    problem_id: number;
+    sample_input: string;
+    sample_output: string;
+    source: string;
+    time_limit: number;
+    title: string;
+    user_id: number;
+    [property: string]: any;
+}
+
+export interface FpsProblemInfo {
+    create_time: string;
+    description: string;
+    input: string;
+    memory_limit: number;
+    output: string;
+    sample_input: string;
+    sample_output: string;
+    time_limit: number;
+    title: string;
+    update_time: string;
+    [property: string]: any;
+}
+
+export interface FpsTestCase {
+    test_input?: string;
+    test_output?: string;
+    [property: string]: any;
+}
+
+export interface FpsSolution {
+    create_time: string;
+    language_id: number;
+    source_code: string;
+    update_time: string;
     [property: string]: any;
 }
